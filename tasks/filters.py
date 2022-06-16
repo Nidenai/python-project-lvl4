5625c2bd993d520999bb087ef438ca5b14af827f
Nidenai <30Dk81Yz>
2022-06-16 19:31:13 +0700

    fix: update translate

--- tasks/filters.py
+++ tasks/filters.py
@@ -2,7 +2,7 @@ from django import forms
 from django.db.models import Value
 from django.db.models.functions import Concat
 from django_filters import FilterSet, BooleanFilter, ChoiceFilter
-
+from django.utils.translation import gettext as _
 from labels.models import Labels
 from statuses.models import Statuses
 from tasks.models import Tasks
@@ -15,10 +15,10 @@ class TaskFilter(FilterSet):
     users = User.objects.values_list(
         'id', Concat('first_name', Value(' '), 'last_name'), named=True
     ).all()
-    status = ChoiceFilter(label='Статус', choices=statuses)
-    assigned_user = ChoiceFilter(label='Исполнитель', choices=users)
-    label = ChoiceFilter(label='Метка', choices=labels)
-    self_task = BooleanFilter(label='Только мои задачи',
+    status = ChoiceFilter(label=_('Status'), choices=statuses)
+    assigned_user = ChoiceFilter(label=_('Executor'), choices=users)
+    label = ChoiceFilter(label=_('Label'), choices=labels)
+    self_task = BooleanFilter(label=_('Only my tasks'),
                               widget=forms.CheckboxInput,
                               method='filter_self',
                               )
