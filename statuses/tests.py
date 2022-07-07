82f054c3608adee5a7e075a45cf0621bfc2bf106
Nidenai <30Dk81Yz>
2022-07-07 23:50:46 +0700

    fix: hexlet_check (0.0.28)

--- statuses/tests.py
+++ statuses/tests.py
@@ -15,6 +15,7 @@ class BaseTestCase(TestCase):
         }
         self.status_page = reverse('statuses:list')
         self.user = User.objects.get(pk=1)
+        self.current_status = Status.objects.get(pk=1)
         return super().setUp()
 
 
@@ -34,4 +35,16 @@ class StatusTestCase(BaseTestCase):
         self.assertRedirects(response, '/statuses/')
         self.assertContains(response, 'Статус успешно создан')
         created_status = Status.objects.get(status_name=status['status_name'])
+        #logger.debug(created_status)
         self.assertEquals(created_status.status_name, 'Test_Status')
+
+    def test_status_delete(self):
+        status_for_delete = {'status_name': 'Status_for_Delete'}
+        self.client.force_login(self.user)
+        self.client.post(reverse('statuses:create'), status_for_delete, follow=True)
+        response = self.client.get(self.status_page)
+        self.assertEqual(response.status_code, 200)
+        current_status = Status.objects.get(pk=3)
+        logger.debug(current_status.id)
+        response = self.client.post(reverse('statuses:delete', args=[current_status.id]), follow=True)
+        self.assertRedirects(response, reverse('statuses:list'))
