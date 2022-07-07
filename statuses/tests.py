8fd011c2b7a66824106b813069d46f188a73ff93
Nidenai <30Dk81Yz>
2022-07-07 21:23:44 +0700

    fix: hexlet_check (0.0.24.4)

--- statuses/tests.py
+++ statuses/tests.py
@@ -2,8 +2,8 @@ from django.test import TestCase
 from django.urls import reverse
 from loguru import logger
 
-from statuses.models import Statuses
 from users.models import User
+from statuses.models import Statuses
 
 
 class BaseTestCase(TestCase):
@@ -13,7 +13,7 @@ class BaseTestCase(TestCase):
         self.status_for_test = {
             'status_name': 'test_status_name'
         }
-        self.status_page = reverse('statuses')
+        self.status_page = reverse('statuses:list')
         self.user = User.objects.get(pk=1)
         return super().setUp()
 
@@ -22,13 +22,16 @@ class BaseTestCase(TestCase):
 class StatusTestCase(BaseTestCase):
     def test_status_page(self):
         response = self.client.get(self.status_page)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
 
 
     def test_status_create(self):
-        status = {'name': 'Test_Status'}
+        status = {'status_name': 'Test_Status'}
         self.client.force_login(self.user)
-        self.client.post(reverse('add_s'), status, follow=True)
         response = self.client.get(self.status_page)
-        response_tasks = list(response.context['statuses'])
-        logger.debug(response_tasks)
+        self.assertEqual(response.status_code, 200)
+        response = self.client.post(reverse('statuses:create'), status, follow=True)
+        self.assertRedirects(response, '/statuses/')
+        self.assertContains(response, 'Статус успешно создан')
+        created_status = Statuses.objects.get(status_name=status['status_name'])
+        self.assertEquals(created_status.status_name, 'Test_Status')
