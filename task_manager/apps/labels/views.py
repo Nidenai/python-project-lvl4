76b5289dda1389218c142d8de4c52751c92303cd
Nidenai <30Dk81Yz>
2022-07-21 21:03:17 +0700

    fix: refactoring fixing: add restrict delete labels

--- task_manager/apps/labels/views.py
+++ task_manager/apps/labels/views.py
@@ -1,9 +1,11 @@
+from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import ListView, UpdateView, CreateView
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView
 
-from task_manager.mixins import CustomDeleteView, CustomLoginRequiredMixin
+from task_manager.mixins import CustomLoginRequiredMixin
 from .forms import LabelForm
 from .models import Labels
 
@@ -29,8 +31,16 @@ class LabelUpdate(CustomLoginRequiredMixin, SuccessMessageMixin, UpdateView):
     success_message = _('Label update succesfully')
 
 
-class LabelDelete(CustomLoginRequiredMixin, CustomDeleteView):
+class LabelDelete(CustomLoginRequiredMixin, SuccessMessageMixin, DeleteView):
     template_name = 'delete.html'
     model = Labels
     success_url = reverse_lazy('labels:list')
     success_message = _('Label delete succesfully')
+    msg_error = _('Невозможно удалить метку, потому что она используется')
+
+    def form_valid(self, form):
+        if self.get_object().labels.all():
+            messages.error(self.request, self.msg_error)
+        else:
+            super(LabelDelete, self).form_valid(form)
+        return redirect(self.success_url)
